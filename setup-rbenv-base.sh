4ec42daef856faafd1ff24a9aeb0cd20f86b5017
Hiroshi Miura <miurahr@linux.com>
2015-04-10 21:43:59 +0900

    support multiple stable versions in script
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup-rbenv-base.sh
+++ setup-rbenv-base.sh
@@ -1,11 +1,30 @@
 #! /bin/bash
 set -e
 
-if [ "RUBY_VER" == "" ]; then
+# How to use
+#
+#  You can use it by sourcing /etc/profile.d/rbenv.sh from your login shell.
+#
+
+# Environment variable for building
+#
+# if RUBY_VERS is not empty, it build all of RUBY versions.
+#  and set RUBY_VER as default.
+#
+#  only RUBY_VER defined, it build with RUBY with RUBY_VER
+#
+#  no varialble defined, it set default as follows:
+
+if [ "${RUBY_VER}" == "" ]; then
   RUBY_VER=1.9.3-p551
 fi
 
-echo "setup rbenv with Ruby ${RUBY_VER}."
+if [ "${RUBY_VERS}" == "" ]; then
+  echo "setup rbenv with Ruby ${RUBY_VER}."
+else
+  echo "setup rbenv with Ruby versions: ${RUBY_VERS}."
+  echo "default version is ${RUBY_VER}."
+fi
 
 env DEBIAN_FRONTEND=noninteractive apt-get update
 env DEBIAN_FRONTEND=noninteractiv apt-get -y install \
@@ -50,8 +69,14 @@ pushd /tmp
   ./install.sh
 popd
 
-# install ruby
+# install ruby versions
+if [ "${RUBY_VERS}" == "" ]; then
 rbenv install ${RUBY_VER}
+else
+  for v in ${RUBY_VERS} ; do
+    rbenv install $v
+  done
+fi
 rbenv global ${RUBY_VER}
 
 # Rehash:
@@ -62,6 +87,7 @@ gem update --system
 gem -v
 gem install bundler --no-rdoc --no-ri
 bundle -v
+
 
 apt-get -y remove \
   bison \
