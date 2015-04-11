ba50f1dea2223279e1c3623084a9fdaecad77591
Hiroshi Miura <miurahr@linux.com>
2015-04-11 10:47:31 +0900

    reduce image size
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup-ruby.sh
+++ setup-ruby.sh
@@ -68,9 +68,12 @@ install_ruby_version () {
 
   rbenv install $ver
   rbenv global $ver
-  gem update --system
+  gem update --system --no-document
   gem install bundler --no-rdoc --no-ri
 }
+
+echo "gem: --no-document" > /etc/gemrc
+echo "gem: --no-document" > /root/.gemrc
 
 # install ruby versions
 if [ "${RUBY_VERS}" == "" ]; then
